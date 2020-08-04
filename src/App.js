4b17b43a403a33df145c2595cb8c90e7b8fd88de
theRod15 <kis3210@outlook.com>
2020-08-04 18:42:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Navbar } from './components/Navbar';
+import { Header } from './components/Header';
+import { TokenBalance } from './components/TokenBalance';
+import { TotalSupply } from './components/TotalSupply';
+import { MoreInfo } from './components/MoreInfo';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navbar />
+      <div className='container'>
+        <Header />
+        <div className='row'>
+          <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
+            <TokenBalance />
+          </div>
+          <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
+            <TotalSupply />
+          </div>
+          <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
+            <MoreInfo />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
