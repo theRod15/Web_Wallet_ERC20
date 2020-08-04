fdddda82d72ae3df52d60bafa6d3746e8d1875f6
theRod15 <kis3210@outlook.com>
2020-08-04 22:59:11 +0500

    Update MoreInfo.js

--- src/components/MoreInfo.js
+++ src/components/MoreInfo.js
@@ -3,20 +3,19 @@ import React from 'react';
 export const MoreInfo = () => {
   return (
     <div className='mainTab'>
-      <span className='Bal-Heading'>More Info</span>
+      <h2 className='Bal-Heading'>More Info</h2>
 
       <form className='mt-2'>
-        <div class='form-group '>
+        <div className='form-group '>
           <label>Token Contract Address</label>
           <input
             type='text'
-            class='form-control form-control-sm'
-            placeholder='0x00000000000000000000000000000000000000000'
-            readonly
+            className='form-control form-control-sm'
+            placeholder='0xf4d3057BC68D4b48714c30Bea06fFA22f1C443dd'
           />
         </div>
-        <small class='form-text '>See on Blockchain</small>
-        <button type='submit' class='btn btn-primary mt-2 btn-block'>
+        <small className='form-text '>See on Blockchain</small>
+        <button type='submit' className='btn btn-primary mt-2 btn-block'>
           Full History
         </button>
       </form>
