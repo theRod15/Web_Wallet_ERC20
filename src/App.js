c9f337c2b6c192e37df5b5bbc74728d61c4413bd
theRod15 <kis3210@outlook.com>
2020-08-05 01:03:35 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Navbar } from './components/Navbar';
 import { Header } from './components/Header';
 import { TokenBalance } from './components/TokenBalance';
-import { TotalSupply } from './components/TotalSupply';
+// import { TotalSupply } from './components/TotalSupply';
 import { MoreInfo } from './components/MoreInfo';
+import { SendToken } from './components/SendToken';
+import { Footer } from './components/Footer';
 import { GlobalProvider } from './context/GlobalContext';
 
 import './App.css';
@@ -12,20 +14,21 @@ function App() {
   return (
     <GlobalProvider>
       <Navbar />
-      <div className='container'>
+      <div className='container py-4'>
         <Header />
         <div className='row'>
           <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
             <TokenBalance />
           </div>
           <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
-            <TotalSupply />
+            {/* <TotalSupply /> */} <SendToken />
           </div>
           <div className='col-lg-4 col-md-4 col-sm-12 col-12'>
             <MoreInfo />
           </div>
         </div>
       </div>
+      <Footer />
     </GlobalProvider>
   );
 }
