bcf96234595c5c321912ffb93a598ac24355a014
theRod15 <kis3210@outlook.com>
2020-08-04 18:42:41 +0500

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Navbar = () => {
+  return (
+    <nav className='navbar nav'>
+      <div className='container'>
+        <span className='navbar-brand'>ERC20 Web Wallet</span>
+        <span className='navbar-text mr-auto'>
+          Using ERC20 Smart Contract Deployed
+        </span>
+      </div>
+    </nav>
+  );
+};
