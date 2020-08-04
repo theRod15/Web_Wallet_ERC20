39373801d7c5fce0cd7ee5368ef3edd7865a0821
theRod15 <kis3210@outlook.com>
2020-08-05 01:03:44 +0500

    Update MoreInfo.js

--- src/components/MoreInfo.js
+++ src/components/MoreInfo.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalContext';
 
 export const MoreInfo = () => {
+  const { totalSupply } = useContext(GlobalContext);
+
   return (
     <div className='mainTab'>
       <h2 className='Bal-Heading'>More Info</h2>
 
-      <form className='mt-2'>
-        <div className='form-group '>
-          <label>Token Contract Address</label>
-          <input
-            type='text'
-            className='form-control form-control-sm'
-            placeholder='0xf4d3057BC68D4b48714c30Bea06fFA22f1C443dd'
-          />
-        </div>
-        <small className='form-text '>See on Blockchain</small>
-        <button type='submit' className='btn btn-primary mt-2 btn-block'>
-          Full History
-        </button>
-      </form>
+      <p>
+        Total Supply: <strong> MAS Token PIAIC</strong>
+      </p>
+      <p>
+        Symbl:<strong> MST</strong>
+      </p>
+      <p>
+        Total Supply: <strong>{totalSupply}</strong>
+      </p>
+      <p>
+        Decimals: <strong>2</strong>
+      </p>
+
+      <small className='form-text '>See on Blockchain</small>
+      <button type='submit' className='btn btn-primary mt-2 btn-block'>
+        Full History
+      </button>
     </div>
   );
 };
