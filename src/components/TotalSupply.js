3f3688bc30574b2bf7dda037c60f7bd11436a9cd
theRod15 <kis3210@outlook.com>
2020-08-04 18:42:46 +0500

    Create TotalSupply.js

---
+++ src/components/TotalSupply.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const TotalSupply = () => {
+  return (
+    <div className='mainTab'>
+      <span className='Bal-Heading'>Total Supply</span>
+      <p className='balAmount'> 10,000.00 MST </p>
+      <div className=''>
+        <button type='submit' class='btn btn-primary btn-block'>
+          Get More info
+        </button>
+      </div>
+    </div>
+  );
+};
