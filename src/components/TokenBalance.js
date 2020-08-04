39359392d219b8012f4c586a59e44fdb1aa3aabf
theRod15 <kis3210@outlook.com>
2020-08-04 22:59:07 +0500

    Update TokenBalance.js

--- src/components/TokenBalance.js
+++ src/components/TokenBalance.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { GlobalContext } from '../context/GlobalContext';
 
 export const TokenBalance = () => {
+  const { web3, tokenBalance } = useContext(GlobalContext);
+
+  const [account, setAccount] = useState('');
+  const [ethBalance, setEthBalance] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      if (web3) {
+        const account = await web3.eth.getCoinbase();
+
+        setAccount(account);
+
+        const ethBalance = await web3.eth.getBalance(account);
+        setEthBalance(web3.utils.fromWei(ethBalance, 'ether'));
+      }
+    })();
+  }, [web3]);
+
   return (
     <div className='mainTab'>
-      <span className='Bal-Heading'>My Balance</span>
-      <p className='balAmount'> 10,000.00 MST </p>
+      <h2>My Balance</h2>
 
-      <form>
-        <div class='form-group '>
-          <label>My Wallet Address</label>
-          <input
-            type='text'
-            class='form-control form-control-sm'
-            placeholder='0x00000000000000000000000000000000000000000'
-            readonly
-          />
-        </div>
-      </form>
+      <p>
+        {Math.abs(ethBalance).toFixed(8)} ETH <br />
+        <span> {Math.abs(tokenBalance).toFixed(2)} MST </span>
+      </p>
+
+      <strong>My Wallet Address</strong>
+
+      <small className='form-text '>{account}</small>
     </div>
   );
 };
