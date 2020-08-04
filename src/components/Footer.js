3facf78681b53036b36679fa4fd42773f4b5937d
theRod15 <kis3210@outlook.com>
2020-08-05 01:03:38 +0500

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <strong>
+          Design by. Muhammad Ashir Ansari <br /> www.corecis.org
+        </strong>
+      </div>
+    </footer>
+  );
+};
