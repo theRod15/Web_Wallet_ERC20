9827d38bd277923e432cc2c13151ca60821ac9be
theRod15 <kis3210@outlook.com>
2020-08-05 01:03:41 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { CONTRACT_ADDRESS } from '../smartContract/web3Config';
 
 export const Header = () => {
   return (
     <div className='header'>
-      <span>MAS Token PIAIC ( MST )</span>
+      <span>MAS Token PIAIC ( MST ) | </span>
+      <div>
+        <span className=''> Contract Address: {CONTRACT_ADDRESS}</span>
+      </div>
     </div>
   );
 };
